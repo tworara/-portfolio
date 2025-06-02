9d0054c2220380bb6d8dd6bee1d919a5e146cc3d
tworara <dlskfk010@naver.com>
2025-06-02 16:40:40 +0900

    0602_1640

--- js/main.js
+++ js/main.js
@@ -4,24 +4,6 @@ $(function () {
 
 
 
-  /* info 가로스크롤 */
-
-  const horizontal = document.querySelector('.horizontal');
-  const sections = gsap.utils.toArray('.horizontal>section');
-  let ani = [];
-  const scrollTween = gsap.to(sections, {
-    xPercent: -100 * (sections.length - 1), //전체 섹션 수만큼 왼쪽으로 밀기
-    ease: 'none', //부드럽게 넘기지 않고 스크롤에 따라 반응
-    scrollTrigger: {
-      trigger: horizontal,
-      start: 'top top', //스크롤이 맨 위에 닿을 때 시작
-      end: () => "+=" + (horizontal.offsetWidth - innerWidth), //스크롤 끝나는 위치 계산
-      pin: true, //해당 부분에서 화면을 고정해서 보여줌
-      scrub: 1, //스크롤에 따라 실시간으로 움직임
-      anticipatePin: 1, // 핀 고정 시 살짝 미리 준비해서 부드럽게
-      invalidateOnRefresh: true, // 새로고침하면 위치 다시 계산해줌
-    }
-  })
 
 
 
@@ -59,6 +41,18 @@ $(function () {
     ease: 'none',
   }, '+=1.8')
 
+
+/* project swiper */
+
+
+let projectSwiper = new Swiper(".projectSwiper", {
+      slidesPerView: "auto",
+      spaceBetween: 30,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+    });
 
 
 
@@ -101,18 +95,24 @@ $('.merit_list_section .merit_list .merit_list_detail').click(function(){
 
   
 
-/* project swiper */
 
+/* design */
 
-let projectSwiper = new Swiper(".projectSwiper", {
-      slidesPerView: "auto",
-      spaceBetween: 30,
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-      },
-    });
+const numberEl = document.querySelector('.design_left');
+const section = document.querySelectorAll('.design');
 
+window.addEventListener('scroll', () => {
+  const center = window.innerHeight / 2;
+
+  section.forEach((section, index) => {
+    const rect = section.getBoundingClientRect();
+
+    if (rect.top <= center && rect.bottom >= center) {
+      const displayNum = (index + 1).toString().padStart(2, '0');
+      numberEl.textContent = displayNum;
+    }
+  });
+});
 
 
 
@@ -120,6 +120,29 @@ let projectSwiper = new Swiper(".projectSwiper", {
   $('.contact_list> li').click(function () {
     $(this).toggleClass('on')
   })
+
+
+  
+  /* info 가로스크롤 */
+
+  const horizontal = document.querySelector('.horizontal');
+  const sections = gsap.utils.toArray('.horizontal>section');
+  let ani = [];
+  const scrollTween = gsap.to(sections, {
+    xPercent: -100 * (sections.length - 1), //전체 섹션 수만큼 왼쪽으로 밀기
+    ease: 'none', //부드럽게 넘기지 않고 스크롤에 따라 반응
+    scrollTrigger: {
+      trigger: horizontal,
+      start: 'top top', //스크롤이 맨 위에 닿을 때 시작
+      end: () => "+=" + (horizontal.offsetWidth - innerWidth), //스크롤 끝나는 위치 계산
+      pin: true, //해당 부분에서 화면을 고정해서 보여줌
+      scrub: 1, //스크롤에 따라 실시간으로 움직임
+      anticipatePin: 1, // 핀 고정 시 살짝 미리 준비해서 부드럽게
+      invalidateOnRefresh: true, // 새로고침하면 위치 다시 계산해줌
+    }
+  })
+
+
 
   AOS.init();
 })
